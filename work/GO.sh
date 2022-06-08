bbc9abcfc2341dcad2981eda628b345a45a59a1b
keith-harrison <keithharrison2000@gmail.com>
2022-06-08 11:30:45 +0100

    11:30 08/06 Fixed Trimal
    Fixed trimal by adding lots of hypons

--- work/GO.sh
+++ work/GO.sh
@@ -6,10 +6,12 @@ python FixID.py
 
 
 
-#Concatenate Identical Taxas
-awk '/^>/ {if(prev!=$0) {prev=$0;printf("\n%s\n",$0);} next;} {printf("%s",$0);} END {printf("\n");}' seqs_without_duplicate.fa > concatenated_seq.fa
+
 #Remove Duplicates
 seqkit rmdup -s fixedtestfile.fa -o seqs_without_duplicate.fa
+seqkit rmdup -n seqs_without_duplicate.fa -o seqs_without_duplicate2.fa
+#Concatenate Identical Taxas
+awk '/^>/ {if(prev!=$0) {prev=$0;printf("\n%s\n",$0);} next;} {printf("%s",$0);} END {printf("\n");}' seqs_without_duplicate2.fa > concatenated_seq.fa
 #Run MAFFT
 mafft --auto concatenated_seq.fa > aligned_seq.fa
 
@@ -19,14 +21,15 @@ trimal -clustal -in aligned_seq.fa -out trimmed_seq.fa
 #Run IQTREE
 #iqtree -nt AUTO  -s aligned_seq.fa
 iqtree -nt AUTO  -s trimmed_seq.fa
-
 cat filename.txt | xargs mkdir
 cat filename.txt | xargs mv *log 
 cat filename.txt | xargs mv *iqtree
 rm ./aligned*
-#rm ./trimmed*
+rm ./trimmed*
 rm ./concatenated*
 rm seqs_without_duplicate.fa
+rm seqs_without_duplicate2.fa
 rm fixed*.fa
 rm filename.txt
+
 
