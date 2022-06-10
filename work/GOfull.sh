5a138a3a6c63c1b565a7a2fdc6ab16e3904bb2f2
keith-harrison <keithharrison2000@gmail.com>
2022-06-10 17:50:56 +0100

    testing start

--- work/GOfull.sh
+++ work/GOfull.sh
@@ -7,19 +7,24 @@ find . -maxdepth 1 -name "fixed*.fa" -exec seqkit rmdup -n {} -o concatenated_re
 
 #Run MAFFT
 mafft --auto concatenated_ready_seq.fa > aligned_seq.fa
-
+sed -i 's/:/_/' aligned_seq.fa
 #Run Trimal*
 trimal -clustal -in aligned_seq.fa -out trimmed_seq.fa
 
-#Run IQTREE
-iqtree -nt AUTO  -s trimmed_seq.fa
-cat filename.txt | xargs mkdir
-cat filename.txt | xargs mv *log 
-cat filename.txt | xargs mv *iqtree
-rm ./aligned*
-rm ./trimmed*
-rm ./concatenated*
-rm fixed*.fa
-rm filename.txt
-rm full*.fa
+
+#Replace erroneous characters that will effect iqtree
+
+
+
+#Run fasttree
+FastTree -quiet trimmed_seq.fa > treefile 
+#cat filename.txt | xargs mkdir
+#cat filename.txt | xargs mv *log 
+#cat filename.txt | xargs mv *iqtree
+#rm ./aligned*
+#rm ./trimmed*
+#rm ./concatenated*
+#rm fixed*.fa
+#rm filename.txt
+#rm full*.fa
 
