3010e2eae08f2d04c84ff47d5daa41b0e35c98fd
Keith Harrison <keithharrison2000@gmail.com>
2022-06-22 14:51:56 +0100

    6 ready

--- work/Master.sh
+++ work/Master.sh
@@ -25,7 +25,7 @@ FastTree -quiet trimmed_seq.fa > newtreefile #dont want to use psuedo as there i
 #get names
 python -c "exec(\"import sys\nimport sys\nfrom ete3 import Tree\nt = Tree('prunedtree.nwk',format=1)\nnames=t.get_leaf_names()\nwith open('listfile.txt', mode='w') as file:\n  for listitem in names:\n    file.write(listitem+str('\\\n'))\")"
 #make concatenated_ready_seq.fa single files fasta
-python single_line.py
+python single_line.py ##fixed concatenated
 #return matching pruned sequences from "master" sequence file
 cat listfile.txt | grep -A 1 --no-group-separator -f - fixedconcatenated_ready_seq.fa > newick_seq_line.fa
 #Now that we have all the sequences in our pruned tree run a regular MAFFT and Trimal followed by IQTREE
