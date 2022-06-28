62dbc70307468de9b3276e8b6660804788f8f3ba
Keith Harrison <keithharrison2000@gmail.com>
2022-06-29 00:46:58 +0100

    due pruned

--- work/workflow/prunedtree.sh
+++ work/workflow/prunedtree.sh
@@ -1,7 +1,7 @@
 #Prune FastTree (Python,MAFFT,Trimal)
 
-#Prune on ITOL export pruned tree as newick format with NO internal node IDs to get prunedtree.nwk and used concatenated_ready_seq.fa 
-python -c "exec(\"import sys\nimport sys\nfrom ete3 import Tree\nt = Tree('prunedtree.nwk',format=1)\nnames=t.get_leaf_names()\nwith open('listfile.txt', mode='w') as file:\n  for listitem in names:\n    file.write(listitem+str('\\\n'))\")"
+#Prune on ITOL export pruned tree as newick format with NO internal node IDs to get pruneddraft.nwk and used concatenated_ready_seq.fa 
+python -c "exec(\"import sys\nimport sys\nfrom ete3 import Tree\nt = Tree('pruneddraft.nwk',format=1)\nnames=t.get_leaf_names()\nwith open('listfile.txt', mode='w') as file:\n  for listitem in names:\n    file.write(listitem+str('\\\n'))\")"
 
 #make concatenated_ready_seq.fa single lined fasta
 python single_line.py 
@@ -13,7 +13,7 @@ cat listfile.txt | grep -A 1 --no-group-separator -xF -f - fixedconcatenated_rea
 
 #Now that we have all the sequences in our pruned tree run a regular MAFFT and Trimal followed by IQTREE
 mafft --auto newick_seq_line.fa > aligned_seq.fa
-trimal -fasta -in aligned_seq.fa -out trimmed_seq.fa
-mv trimmed_seq.fa pruned_tree.fasta
-
-nohup iqtree -nt AUTO -s pruned_tree.fasta -bb 1000 -pre pruned &
+trimal -fasta -gappyout -in aligned_seq.fa -out trimmed_seq_gappy.fasta
+trimal -fasta  -in aligned_seq.fa -out trimmed_seq.fasta
+nohup iqtree -nt AUTO -s trimmed_seq.fasta -bb 1000 -pre pruned &
+nohup iqtree -nt AUTO -s trimmed_seq_gappy.fasta -bb 1000 -pre prunedgappy &
