9d629164cd4424b8dab4425934a26c0db5cf1ecc
Keith Harrison <keithharrison2000@gmail.com>
2022-06-29 17:45:06 +0100

    changes

--- work/workflow/MMETSP/mmetsp.py
+++ work/workflow/MMETSP/mmetsp.py
@@ -1,35 +1,43 @@
-
-
+from taxonomy_ranks import TaxonomyRanks
 import csv
-from subprocess import ABOVE_NORMAL_PRIORITY_CLASS
 import pandas as pd
 df = pd.read_csv("MMETSPinfo.csv")
-#print first column 
-keep = df.iloc[:,0]
-#delete files in current directory that are not in the list
-'''
-if we want to remove mmetsp files that are bad we can use the following code
-import os
-for file in os.listdir("."):
-    
 
-    if file not in keep.tolist():
-        os.remove(file)
-        print("removed: " + file)
-'''
-print("unid." not in "unid. Prasinophyte Clade VIIA1")
-#print each record
-#write each row's genus to a file
-with open("genus.txt", "w") as f:
-    for index, row in df.iterrows():
-        #only write if not Undescribed or contains the string 'unid'
-        #print(row["Genus"])
-        if row["Genus"] != "Undescribed" and ("unid" not in row["Genus"]) and ('nov.' not in row["Genus"]) and ("Unid." not in row["Genus"]) and ("Unknown" not in row["Genus"]):
-            f.write(row["Genus"] + "\n")
+#loop through rows in df and run rank_taxon on them
+for index, row in df.iterrows():
+    #make new column for taxon
+    #print size of df
+    try:
+        rank_taxon = TaxonomyRanks(row['Genus'])
+        rank_taxon.get_lineage_taxids_and_taxanames()
+        taxon = str(list(rank_taxon.lineages)[0])
+        df.loc[index, "Taxon"] = taxon
+    except:
+        df.loc[index, "Taxon"] = "NA"
 
+#make a list of MMETSP ids that have a valid taxon column
+keep_list = []
+for index, row in df.iterrows():
+    if row['Taxon'] != "NA":
+        keep_list.append(row['ID'])
+#concatenate files together that have the same taxon
+#group MMETSP ids by taxon not including NA
+df = df[df["Taxon"] != "NA"]
 
-#zip togehter mmetsp ids and the genus they belong to
-#remove those at match th eif statement above
-#Remove mmetsp files that do not match 
+#add .pep suffix to ID column
+df["ID"] = df["ID"] + ".pep"
+#get all unique taxas
+taxas = df["Taxon"].unique()
 
+for i in taxas:
+    #write file called i.pep
+    with open(i + ".pep", "w") as f:
+        #write contents to file if the taxon is in the row
+        for index, row in df.iterrows():
+            if row["Taxon"] == i:
+                #open file from row["ID"]
+                with open(row["ID"]) as g:
+                    #write contents of g to f
+                    for line in g:
+                        f.write(line)
 
